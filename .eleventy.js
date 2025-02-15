6efce212ad72e60b6866ceec4426d626bc0b4e8a
Kristján Oddsson <koddsson@gmail.com>
2025-02-15 08:17:07 +0000

    markdown headings and plugin index

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,44 @@
+import markdownit from "markdown-it";
 import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
+
+function uncollide(ids, id) {
+  if (!ids[id]) return id;
+  let i = 1;
+  while (ids[id + "-" + i]) i++;
+  return id + "-" + i;
+}
+
+function setAttr(token, attr, value, options) {
+  const idx = token.attrIndex(attr);
+
+  if (idx === -1) {
+    token.attrPush([attr, value]);
+  } else if (options && options.append) {
+    token.attrs[idx][1] = token.attrs[idx][1] + " " + value;
+  } else {
+    token.attrs[idx][1] = value;
+  }
+}
+
+function namedHeadings(state) {
+  console.log(state.tokens.entries());
+  for (const [i, token] in state.tokens.entries()) {
+    console.log({ i, token });
+    if (token.type === "heading_open") {
+      console.log("here");
+      const text = md.renderer.render(state.tokens[i + 1].children, md.options);
+      console.log({ text });
+      const id = kebabcase(unidecode(text));
+      const uniqId = uncollide(ids, id);
+      ids[uniqId] = true;
+      setAttr(token, "id", uniqId);
+    }
+  }
+}
+
+const md = markdownit().use(function (md) {
+  md.core.ruler.push("named_headings", namedHeadings);
+});
 
 export default function (eleventyConfig) {
   // Enable syntax highlighting
